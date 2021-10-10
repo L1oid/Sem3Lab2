f05717c75a63c95038a2cf4ecdffc14e019d2098
L1oid <71085120+L1oid@users.noreply.github.com>
2021-10-10 12:55:21 +0700

    Add files via upload

--- Main.cpp
+++ Main.cpp
@@ -6,12 +6,6 @@
 #include <chrono>
 using namespace std;
 
-int charToNum(char);
-bool DimArr(int&);
-bool GetFile(int*&);
-void allinfo(int&, int, int*&);
-void sortinfo(int&, int, int*&);
-
 class datetime
 {
 public:
@@ -21,10 +15,11 @@ public:
 	int day;
 	int month;
 	int year;
-	void Out();
-	void NextDate(int& i, int n, int*& arr, int key);
-	void PrevDate(int& i, int n, int*& arr);
+
+	datetime NextDate(int& i, int n, datetime*& arrd, int key);
+	datetime PrevDate(int& i, int n, datetime*& arrd);
 	void SysTime();
+	int charToNum(char str);
 };
 
 void datetime::SysTime()
@@ -40,22 +35,22 @@ void datetime::SysTime()
 	else day = 0;
 	day = day * 10 + charToNum(sysDate[9]);
 
-	if(sysDate[4] == 'J') 
+	if (sysDate[4] == 'J')
 	{
 		if (sysDate[5] == 'a') month = 1;
-		else 
+		else
 		{
 			if (sysDate[6] == 'n') month = 6;
 			else month = 7;
 		}
 	}
 	else if (sysDate[4] == 'F') month = 2;
-	else if (sysDate[4] == 'M') 
+	else if (sysDate[4] == 'M')
 	{
 		if (sysDate[6] == 'r') month = 3;
 		else month = 5;
 	}
-	else if (sysDate[4] == 'A') 
+	else if (sysDate[4] == 'A')
 	{
 		if (sysDate[5] == 'p') month = 4;
 		else month = 8;
@@ -80,203 +75,7 @@ void datetime::SysTime()
 	second += charToNum(sysDate[18]);
 }
 
-void datetime::Out()
-{
-	cout << "Date: " << day << "." << month << "." << year << " Time: " << hour << ":" << minute << ":" << second;
-}
-
-void datetime::NextDate(int& i, int n, int*& arr, int key)
-{
-	datetime temp;
-	if (key == 1)
-	{
-		i = i + 6;
-		if (i < n)
-		{
-			cout << " - ";
-			day = arr[i];
-			month = arr[i + 1];
-			year = arr[i + 2];
-			hour = arr[i + 3];
-			minute = arr[i + 4];
-			second = arr[i + 5];
-			Out();
-		}
-	}
-	if (key == 2)
-	{
-		temp.day = arr[i];
-		temp.month = arr[i + 1];
-		temp.year = arr[i + 2];
-		temp.hour = arr[i + 3];
-		temp.minute = arr[i + 4];
-		temp.second = arr[i + 5];
-		i = i + 6;
-		if (i < n)
-		{
-			day = arr[i];
-			month = arr[i + 1];
-			year = arr[i + 2];
-			hour = arr[i + 3];
-			minute = arr[i + 4];
-			second = arr[i + 5];
-			if (temp.year < year || temp.year > year)
-			{
-				temp.Out();
-				cout << " - ";
-				Out();
-				cout << endl;
-			}
-		}
-	}
-}
-
-void datetime::PrevDate(int& i, int n, int*& arr)
-{
-	if (i - 6 != 0)
-	{
-		i = i - 12;
-		cout << " - ";
-		day = arr[i];
-		month = arr[i + 1];
-		year = arr[i + 2];
-		hour = arr[i + 3];
-		minute = arr[i + 4];
-		second = arr[i + 5];
-		i = i + 12;
-		Out();
-	}
-}
-
-struct timedelta 
-{
-	int second;
-	int minute;
-	int hour;
-	int day;
-	int month;
-	int year;
-
-	void Out();
-	void minus(datetime t1, datetime t2);
-	datetime timePlus(datetime t1);
-	void DeltaPlusDelta(timedelta d);
-};
-
-void timedelta::minus(datetime t1, datetime t2)
-{
-	second = t1.second - t2.second;
-	minute = t1.minute - t2.minute;
-	hour = t1.hour - t2.hour;
-	day = t1.day - t2.day;
-	month = t1.month - t2.month;
-	year = t1.year - t2.year;
-}
-
-datetime timedelta::timePlus(datetime t1) 
-{
-	t1.second -= second;
-	t1.minute -= minute;
-	t1.hour -= hour;
-	t1.day -= day;
-	t1.month -= month;
-	t1.year -= year;
-	return t1;
-}
-
-void timedelta::DeltaPlusDelta(timedelta t2) 
-{
-	second += t2.second;
-	minute += t2.minute;
-	hour += t2.hour;
-	day += t2.day;
-	month += t2.month;
-	year += t2.year;
-}
-
-void timedelta::Out()
-{
-	cout << "Date: " << day << "." << month << "." << year << " Time: " << hour << ":" << minute << ":" << second;
-}
-
-int main()
-{
-	datetime data;
-	datetime d1, d2;
-	timedelta delta, t2;
-	int n = 0, i = 0;
-	DimArr(n);
-	int *arr = new int[n];
-	GetFile(arr);
-	int key = 0;
-	do
-	{
-		cout << "1) Info output" << endl
-			<< "2) Op. 2 Info output" << endl
-			<< "3) System Date" << endl
-			<< "4) Timedelta" << endl
-			<< "0) Exit" << endl;
-		cout << endl << "Select an action: ";
-		cin >> key;
-		cout << endl;
-		switch (key)
-		{
-		case 1:
-			i = 0;
-			allinfo(i, n, arr);
-			cout << endl;
-			break;
-		case 2:
-			i = 0;
-			sortinfo(i, n, arr);
-			cout << endl;
-			break;
-		case 3:
-			data.SysTime();
-			data.Out();
-			cout << endl;
-			break;
-		case 4:
-			d1.day = arr[i];
-			d1.month = arr[i + 1];
-			d1.year = arr[i + 2];
-			d1.hour = arr[i + 3];
-			d1.minute = arr[i + 4];
-			d1.second = arr[i + 5];
-			d2.day = arr[i + 6];
-			d2.month = arr[i + 7];
-			d2.year = arr[i + 8];
-			d2.hour = arr[i + 9];
-			d2.minute = arr[i + 10];
-			d2.second = arr[i + 11];
-			delta.minus(d1, d2);
-			delta.Out();
-			cout << endl;
-			d1 = delta.timePlus(d1);
-			d1.Out();
-			cout << endl;
-			t2.day = 3;
-			t2.month = 5;
-			t2.year = -1;
-			t2.hour = -4;
-			t2.minute = 5;
-			t2.second = -15;
-			delta.DeltaPlusDelta(t2);
-			delta.Out();
-			cout << endl;
-			break;
-		default:
-			if (key != 0)
-			{
-				cout << "There is no such action !" << endl << endl;
-			}
-			break;
-		}
-	} while (key != 0);
-	delete[] arr;
-}
-
-int charToNum(char str) 
+int datetime::charToNum(char str)
 {
 	if (str == '0') return 0;
 	else if (str == '1') return 1;
@@ -292,36 +91,212 @@ int charToNum(char str)
 	return 404;
 }
 
-void allinfo(int& i, int n, int*& arr)
+datetime datetime::NextDate(int& i, int n, datetime*& arrd, int key)
 {
-	datetime data;
-	while (i < n)
+	datetime d1 = { 0,0,0,0,0,0 };
+	if (key == 1)
 	{
-		data.day = arr[i];
-		data.month = arr[i + 1];
-		data.year = arr[i + 2];
-		data.hour = arr[i + 3];
-		data.minute = arr[i + 4];
-		data.second = arr[i + 5];
-		data.Out();
-		data.NextDate(i, n, arr, 1);
-		data.PrevDate(i, n, arr);
+		i = i + 1;
+		if (i < (n / 6))
+		{
+			return arrd[i];
+		}
+		return d1;
+	}
+	if (key == 2)
+	{
+		if (i < (n / 6) - 1)
+		{
+			if (arrd[i].year != arrd[i + 1].year)
+			{
+				return arrd[i + 1];
+			}
+		}
+		return d1;
+	}
+	return d1;
+}
+
+datetime datetime::PrevDate(int& i, int n, datetime*& arrd)
+{
+	datetime d1 = { 0,0,0,0,0,0 };
+	if (i - 1 != 0)
+	{
+		return arrd[i - 2];
+	}
+	return d1;
+}
+
+class timedelta
+{
+public:
+	int second;
+	int minute;
+	int hour;
+	int day;
+	int month;
+	int year;
+
+	void minus(datetime t1, datetime t2);
+	datetime timePlus(datetime t1);
+	void DeltaPlusDelta(timedelta d);
+};
+
+void timedelta::minus(datetime t1, datetime t2)
+{
+	second = t1.second - t2.second;
+	minute = t1.minute - t2.minute;
+	hour = t1.hour - t2.hour;
+	day = t1.day - t2.day;
+	month = t1.month - t2.month;
+	year = t1.year - t2.year;
+}
+
+datetime timedelta::timePlus(datetime t1)
+{
+	t1.second -= second;
+	t1.minute -= minute;
+	t1.hour -= hour;
+	t1.day -= day;
+	t1.month -= month;
+	t1.year -= year;
+	return t1;
+}
+
+void timedelta::DeltaPlusDelta(timedelta t2)
+{
+	second += t2.second;
+	minute += t2.minute;
+	hour += t2.hour;
+	day += t2.day;
+	month += t2.month;
+	year += t2.year;
+}
+
+class application
+{
+	datetime temp, data, d1 , d2;
+	timedelta delta, t2;
+	int* arr;
+	datetime* arrd;
+	int n = 0, i = 0;
+	int key = 0;
+public:
+	application()
+	{
+
+	}
+	application(datetime &temp, int& k, int*& arr)
+	{
+		
+		temp.day = arr[k];
+		temp.month = arr[k + 1];
+		temp.year = arr[k + 2];
+		temp.hour = arr[k + 3];
+		temp.minute = arr[k + 4];
+		temp.second = arr[k + 5];
+	}
+	application(int n, int*& arr, datetime*& arrd)
+	{
+		arr = new int[n];
+		arrd = new datetime[n / 6];
+	}
+	application(datetime& d1, datetime& d2, datetime*& arrd)
+	{
+
+		d1 = arrd[i];
+		d2 = arrd[i + 1];
+	}
+	void start();
+	bool DimArr(int& n);
+	void allinfo(int& i, int n, datetime*& arrd);
+	void sortinfo(int& i, int n, datetime*& arrd);
+	bool GetFile(int*& arr, datetime*& arrd);
+};
+
+void application::start()
+{
+	DimArr(n);
+	application(n, arr, arrd);
+	GetFile(arr, arrd);
+	do
+	{
+		cout << "1) Info output" << endl
+			<< "2) Op. 2 Info output" << endl
+			<< "3) System Date" << endl
+			<< "4) Timedelta" << endl
+			<< "0) Exit" << endl;
+		cout << endl << "Select an action: ";
+		cin >> key;
 		cout << endl;
-	}
+		switch (key)
+		{
+		case 1:
+			i = 0;
+			allinfo(i, n, arrd);
+			cout << endl;
+			break;
+		case 2:
+			i = 0;
+			sortinfo(i, n, arrd);
+			cout << endl;
+			break;
+		case 3:
+			data.SysTime();
+			cout << data.day << ":";
+			cout << data.month << ":";
+			cout << data.year << " ";
+			cout << data.hour << ":";
+			cout << data.minute << ":";
+			cout << data.second;
+			cout << endl << endl;
+			break;
+		case 4:
+			application(d1, d2, arrd);
+			delta.minus(d1, d2);
+			cout << delta.day << ":";
+			cout << delta.month << ":";
+			cout << delta.year << " ";
+			cout << delta.hour << ":";
+			cout << delta.minute << ":";
+			cout << delta.second;
+			cout << endl;
+			d1 = delta.timePlus(d1);
+			cout << d1.day << ":";
+			cout << d1.month << ":";
+			cout << d1.year << " ";
+			cout << d1.hour << ":";
+			cout << d1.minute << ":";
+			cout << d1.second;
+			cout << endl;
+			t2.day = 3;
+			t2.month = 5;
+			t2.year = -1;
+			t2.hour = -4;
+			t2.minute = 5;
+			t2.second = -15;
+			delta.DeltaPlusDelta(t2);
+			cout << delta.day << ":";
+			cout << delta.month << ":";
+			cout << delta.year << " ";
+			cout << delta.hour << ":";
+			cout << delta.minute << ":";
+			cout << delta.second;
+			cout << endl << endl;
+			break;
+		default:
+			if (key != 0)
+			{
+				cout << "There is no such action !" << endl << endl;
+			}
+			break;
+		}
+	} while (key != 0);
+	delete[] arr;
 }
 
-void sortinfo(int& i, int n, int*& arr)
+bool application::DimArr(int& n)
 {
-	datetime data;
-	while (i < n)
-	{
-		data.NextDate(i, n, arr, 2);
-	}
-}
-
-bool DimArr(int& n)
-{
-	datetime data;
 	int temp;
 	ifstream F("Date.txt");
 	if (!F)
@@ -337,21 +312,95 @@ bool DimArr(int& n)
 	return true;
 }
 
-bool GetFile(int*& arr)
+void application::allinfo(int& i, int n, datetime*& arrd)
 {
-	datetime data;
-	int temp, i = 0;
+	while (i < (n / 6))
+	{
+		cout << arrd[i].day << ":";
+		cout << arrd[i].month << ":";
+		cout << arrd[i].year << ":";
+		cout << arrd[i].second << ":";
+		cout << arrd[i].minute << ":";
+		cout << arrd[i].hour;
+		d1 = data.NextDate(i, n, arrd, 1);
+		if (d1.day > 0)
+		{
+			cout << " - ";
+			cout << d1.day << ":";
+			cout << d1.month << ":";
+			cout << d1.year << ":";
+			cout << d1.second << ":";
+			cout << d1.minute << ":";
+			cout << d1.hour;
+		}
+		d1 = data.PrevDate(i, n, arrd);
+		if (d1.day > 0)
+		{
+			cout << " - ";
+			cout << d1.day << ":";
+			cout << d1.month << ":";
+			cout << d1.year << ":";
+			cout << d1.second << ":";
+			cout << d1.minute << ":";
+			cout << d1.hour;
+		}
+		cout << endl;
+	}
+}
+
+void application::sortinfo(int& i, int n, datetime*& arrd)
+{
+	while (i < (n / 6) - 1)
+	{
+		data = data.NextDate(i, n, arrd, 2);
+		if (data.day > 0)
+		{
+			cout << arrd[i].day << ":";
+			cout << arrd[i].month << ":";
+			cout << arrd[i].year << ":";
+			cout << arrd[i].second << ":";
+			cout << arrd[i].minute << ":";
+			cout << arrd[i].hour;
+			cout << " - ";
+			cout << data.day << ":";
+			cout << data.month << ":";
+			cout << data.year << ":";
+			cout << data.second << ":";
+			cout << data.minute << ":";
+			cout << data.hour;
+			cout << endl;
+		}
+		i = i + 1;
+	}
+}
+
+bool application::GetFile(int*& arr, datetime*& arrd)
+{
+	int temp1, i = 0;
 	ifstream F("Date.txt");
 	if (!F)
 	{
 		cout << "Cant find file" << endl;
 		return false;
 	}
-	while (F >> temp)
+	while (F >> temp1)
 	{
-		arr[i] = temp;
+		arr[i] = temp1;
 		i++;
+	}
+	int k = 0;
+	for (int j = 0; j < (i / 6); j++)
+	{
+		application(temp, k, arr);
+		k = k + 6;
+		arrd[j] = temp;
 	}
 	F.close();
 	return true;
+}
+
+int main()
+{
+	application app;
+	app.start();
 }
